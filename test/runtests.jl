29107fe760231bc798f88070021e6ffb5a3cbc39
Jesús Murga Moreno <jmurgamoreno@pop-os.localdomain>
2023-09-18 10:48:49 +0200

    version 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Test
 # Running MKtest approximation with default parameters
 @testset "MKtest.jl" begin
 
-	adap  = MKtest.parameters(N=1000,n=661,gam_dfe=-457,gam_flanking=-1000,al_low=0.2,B_bins=[0.2,0.999])
+	adap  = MKtest.parameters(N=1000,n=661,gam_dfe=-457,gam_flanking=-1000,al_low=0.2,B=0.999)
 	binom = MKtest.binom_op(adap)
 	MKtest.set_θ!(adap)
 	MKtest.set_ppos!(adap)
 
-	@test adap.ppos_h == 0.00020733640485526953
-	@test adap.θ_flanking == 3.0015010007506417e-6
+	@test adap.ppos_h == 0.00020733640485526956
+	@test adap.θ_flanking == 3.001501000750676e-6
 
 end
